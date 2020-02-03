69798f1c26d6052d5c26e8253df825912092c490
zachtyoung <zachyoung@Zachs-MacBook-Pro.local>
2020-02-03 16:15:45 -0600

    Sanity Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Route, Switch } from "react-router-dom";
 import './App.css';
+import PrivateRoute from './utils/privateRoute'
+import Landing from './components/Landing'
+import Login from './components/Login'
+import SignUp from './components/SignUp'
+import Dashboard from './components/Dashboard'
+
 
 function App() {
   return (
     <div className="App">
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={SignUp} />
+          <PrivateRoute path="/dashboard" component={Dashboard} />
+        </Switch>
     </div>
   );
 }
