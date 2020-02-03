391c81ff529060e818830aa9f20485a560b3d381
zachtyoung <zachyoung@Zachs-MacBook-Pro.local>
2020-02-03 16:55:21 -0600

    Sanity Commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+const Navbar = () => {
+    return (
+      <div className='nav-wrapper'>
+          <span>App name goes here</span>
+          <ul className='nav-items'>
+              <li>Home</li>
+              <li>Game</li>
+              <li>Login/Logout</li>
+          </ul>
+      </div>
+    );
+  };
+  
+  export default Navbar;
