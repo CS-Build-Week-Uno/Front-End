2269d49424a59560d0bf28807cde7e09d6b7c32a
ebcitron <ebcitron@gmail.com>
2020-02-06 21:58:44 -0600

    Working

--- src/store/reducers/playerReducer.js
+++ src/store/reducers/playerReducer.js
@@ -1,22 +1,51 @@
-
 const initialState = {
-    // direction: 'S',
-    // alive: true,
-    position: [0, 0],
-
-}
-
+  // direction: 'S',
+  // alive: true,
+  position: [0, 0]
+};
 
 const playerReducer = (state = initialState, action) => {
-    console.log("playerReducer:  State: ", state, " action: ", action);
-    switch(action.type) {
-        case 'MOVE_PLAYER':
-            return{
-                ...action.payload
-            }
-        default:
-            return state
-    }
-}
+  console.log("playerReducer:  State: ", state, " action: ", action);
+  switch (action.type) {
+    case "MOVE_PLAYER":
+      console.log("Move Player Payload: ", action.payload);
+      return {
+        ...action.payload
+      };
 
-export default playerReducer
+    case "MOVE_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+        location: { ...state.location, ...action.payload },
+        responseLog:
+          action.payload.description !==
+          state.responseLog[state.responseLog.length - 1].description
+            ? [
+                ...state.responseLog,
+                {
+                  title: action.payload.title,
+                  description: action.payload.description
+                }
+              ]
+            : state.responseLog
+      };
+    case "MOVE_ERROR":
+      return {
+        ...state,
+        error: action.payload,
+        responseLog: [
+          ...state.responseLog,
+          {
+            title: action.payload.title,
+            description: action.payload.description
+          }
+        ]
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default playerReducer;
