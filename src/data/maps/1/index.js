6b3bf8f30ee1d9077bcce745da946465ab591c74
Derek Etman <dereketman@gmail.com>
2020-02-07 08:51:14 -0800

    + imported missing west tile +Made default map zigzag

--- src/data/maps/1/index.js
+++ src/data/maps/1/index.js
@@ -1,15 +1,16 @@
 export const tiles = [
-    ["n","s","e","w","solid","solid","solid","solid","solid","solid"],
-    ["ns","solid","solid","solid","solid","solid","solid","solid","solid","solid"],
-    ["ns","nse","nse","nse","nse","nse","nse","nse","nse","nse"],
-    ["ns","nse","nse","nse","nse","nse","nse","nse","nse","nse"],
-    ["ns","nse","nse","nse","nse","nse","nse","nse","nse","nse"],
-    ["nse","nse","nse","nse","nse","nse","nse","nse","nse","nse"],
-    ["solid","e","swe","ne","solid","solid","solid","solid","solid","solid"],
-    ["ns","ns","ns","ns","ns","ns","ns","ns","ns","ns"],
-    ["ns","ns","ns","ns","ns","ns","ns","ns","ns","ns"],
-    ['n',"solid","solid","solid","solid","solid","solid","solid","solid","solid"],
+    ["e","we","we","we","we","we","we","we","we","ws"],
+    ["se","we","we","we","we","we","we","we","we","nw"],
+    ["ne","we","we","we","we","we","we","we","we","ws"],
+    ["se","we","we","we","we","we","we","we","we","nw"],
+    ["ne","we","we","we","we","we","we","we","we","ws"],
+    ["se","we","we","we","we","we","we","we","we","nw"],
+    ["ne","we","we","we","we","we","we","we","we","ws"],
+    ["se","we","we","we","we","we","we","we","we","nw"],
+    ["ne","we","we","we","we","we","we","we","we","ws"],
+    ['e',"we","we","we","we","we","we","we","we","w"],
 ]
+
 
 /*
 
