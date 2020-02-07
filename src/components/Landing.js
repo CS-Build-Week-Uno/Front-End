30db5f316d1c6147d52bf9073ef6970d7cab66e8
zachtyoung <zachyoung@Zachs-MacBook-Pro.local>
2020-02-06 18:47:09 -0600

    wip

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,23 +1,14 @@
 import React from "react";
-import colin from "../assets/colin.jpeg";
-import zach from "../assets/zach.png";
-import eli from "../assets/eli.jpeg";
-import derek from "../assets/derek.jpeg";
-import jeff from "../assets/jeff.jpeg";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { Link } from "react-router-dom";
 
 
 const Landing = () => {
   return (
     <>
       <h3>
-        Hello World, welcome to our game. This is a description about our game
-        that you are reading. Isn't it a wonderful game? We think so. It's about
-        stuff and things and some more things and other stuff, you know, the
-        usual. Take a second to enjoy this beauty becuase we worked hard on it.
-        Okay... we worked on it.{" "}
+        Welcome to Don't Die. The goal of this game is, you guessed it, to not die. That's not the only thing you'll be guessing. To make to the end of the course you must guess the correct sequence of numbers to pass each section and make your way to the end. Good luck!
       </h3>
+      <Link to="/dashboard" className='play'>Play Now</Link>
     </>
   );
 };
