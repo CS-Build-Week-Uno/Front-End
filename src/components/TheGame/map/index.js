ec2526dcea13227501adc200d920c463876759c5
Derek Etman <dereketman@gmail.com>
2020-02-07 00:34:33 -0800

    + Switch cases in map for directions + Map displays based on arrray matrix + Co-authored-by: Zach Young <ztyoung59@yahoo.com>

--- src/components/TheGame/map/index.js
+++ src/components/TheGame/map/index.js
@@ -1,17 +1,67 @@
-import React from 'react';
+import React from "react";
 
-function Map(props){
-    return(
-        <div
-        style={{
-            width:'630px',
-            height:'630px',
-            top:'0px',
-            left:'0px'
-        }} 
-        />
-    )
+
+function MapTile(props) {
+	return <div className={`tile ${getTileSprite(props.tile)}`}></div>;
 }
 
+function MapRow(props) {
+	return props.tiles.map(tile => <MapTile tile={tile} />);
+}
+
+function getTileSprite(type) {
+	switch (type) {
+		case "e":
+			return "e";
+		case "n":
+			return "n";
+		case "ne":
+			return "ne";
+		case "ns":
+			return "ns";
+		case "nse":
+			return "nse";
+		case "nw":
+			return "nw";
+		case "nwe":
+			return "nwe";
+		case "nws":
+			return "nws";
+		case "nwse":
+			return "nwse";
+		case "s":
+			return "s";
+		case "se":
+			return "se";
+		case "solid":
+			return "solid";
+		case "w":
+			return "w";
+		case "we":
+			return "we";
+		case "ws":
+			return "ws";
+		case "wse":
+			return "wse";
+	}
+}
+
+function Map(props) {
+	return (
+		<div
+			style={{
+				position: "relative",
+				width: "640px",
+				height: "640px",
+				top: "0px",
+				left: "0px"
+			}}
+		>
+			{props.tiles.map(row => (
+				<MapRow tiles={row} />
+			))}
+		</div>
+	);
+}
 
 export default Map;
